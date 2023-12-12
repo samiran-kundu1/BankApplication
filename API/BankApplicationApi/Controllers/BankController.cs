9923212fa084ea812bd723122f86a55a4bb7a2e7
samiran-kundu1 <40227607+samiran-kundu1@users.noreply.github.com>
2023-12-12 15:29:09 +0530

    Add files via upload

---
+++ API/BankApplicationApi/Controllers/BankController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Interface;
+using BusinessLayer.Service;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApplicationApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankController : ControllerBase
+    {
+        #region Private Members
+
+        //Dependency Injection
+        IBankService _bankService;
+
+        #endregion
+
+        public BankController(IBankService bankService)
+        {
+            _bankService = bankService;
+        }
+
+        [HttpGet(Name = "GetBankUsers")]
+        public IActionResult GetUsers()
+        {
+            return Ok(_bankService.GetUserList());
+            //return Ok(bankService.GetUserList());
+        }
+
+        [HttpGet]
+        //[HttpGet("{id}")]
+        [Route("GetUserAccount")]
+        public IActionResult GetUserAccount(int userId)
+        {
+            return Ok(_bankService.GetUserAccountList(userId));
+        }
+        
+        [HttpPost]
+        //[HttpGet("{id}")]
+        [Route("CreateAccount")]
+        public IActionResult CreateUserAccount(int userId,decimal amount = 100)
+        {
+            return Ok(_bankService.CreateUserAccount(userId, amount));
+        }
+        
+        [HttpDelete]
+        //[HttpGet("{id}")]
+        [Route("DeleteAccount")]
+        public IActionResult DeleteUserAccount([FromBody] Account account)
+        {
+            return Ok(_bankService.DeleteUserAccount(account.UserId, account.AccountId));
+        }
+    }
+}
---
+++ API/BankApplicationApi/Controllers/TranSactionController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Interface;
+using BusinessLayer.Service;
+using DAL.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApplicationApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TranSactionController : ControllerBase
+    {
+        #region Private Members
+
+        //TransactionService transactionService = new TransactionService();
+        ITransactionService _transactionService;
+        #endregion
+
+        public TranSactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpPut]
+        [Route("Deposit")]
+        public IActionResult DepositAmountInAccount(int userId, int acctId, decimal amount)
+        {
+                var balance = _transactionService.DepositAmountInAccount(userId, acctId, amount);
+                return Ok(balance);
+        }
+        
+        [HttpPut]
+        [Route("WithDraw")]
+        public IActionResult WithDrawAmountInAccount(int userId, int acctId, decimal amount)
+        {
+                var balance = _transactionService.WithDrawAmountInAccount(userId, acctId, amount);
+                return Ok(balance);
+        }
+    }
+}
